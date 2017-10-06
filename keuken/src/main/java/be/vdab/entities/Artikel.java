9be48da58f72915734453eae0aa9ccee48530816
JansenQuinten <quiinten@gmail.com>
2017-10-06 08:23:53 +0200

    Alles

---
+++ keuken/src/main/java/be/vdab/entities/Artikel.java
@@ -0,0 +1,41 @@
+package be.vdab.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "artikels")
+public class Artikel implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	private long id;
+	private String naam;
+	private BigDecimal aankoopprijs;
+	private BigDecimal verkoopprijs;
+
+	public long getId() {
+		return id;
+	}
+
+	public String getNaam() {
+		return naam;
+	}
+
+	public BigDecimal getAankoopprijs() {
+		return aankoopprijs;
+	}
+
+	public BigDecimal getVerkoopprijs() {
+		return verkoopprijs;
+	}
+
+	public BigDecimal getWinstPercentage() {
+		return verkoopprijs.subtract(aankoopprijs).divide(aankoopprijs, 2, RoundingMode.HALF_UP)
+				.multiply(BigDecimal.valueOf(100));
+	}
+}
---
+++ keuken/src/main/webapp/WEB-INF/tags/menu.tag
@@ -0,0 +1,19 @@
+<%@ tag language="java" pageEncoding="ISO-8859-1"%>
+<%@taglib prefix='c' uri='http://java.sun.com/jsp/jstl/core'%>
+<nav>
+	<ul>
+		<li><a href="<c:url value='/artikels/zoekenopnummer.htm'/>">Artikel
+				zoeken op nummer</a></li>
+		<li><a href="<c:url value='/artikels/toevoegen.htm'/>">Artikel
+				toevoegen </a></li>
+		<li><a href="<c:url value='/artikels/zoekenopnaam.htm'/>">Artikels
+				zoeken op naam</a></li>
+		<li><a href="<c:url value='/artikels/prijsverhoging.htm'/>">Prijsverhoging
+		</a></li>
+		<li><a href="<c:url value='/artikels/kortingen.htm'/>">Artikelkortingen
+		</a></li>
+		<li><a href="<c:url value='/artikels/perartikelgroep.htm'/>">Artikels
+				per artikelgroep</a></li>
+		<li><a href="<c:url value='/artikels.htm'/>">Artikellijst</a></li>
+	</ul>
+</nav>
